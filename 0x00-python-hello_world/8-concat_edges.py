49c18814ee3c8ab2a7702fb06146a9dcaaa643f9
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-11-27 20:18:16 +0100

    string concat

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
-#!/bin/python3
+#!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming langua\
 ge that combines remarkable power with very clear syntax"
 str = str[39:66]
