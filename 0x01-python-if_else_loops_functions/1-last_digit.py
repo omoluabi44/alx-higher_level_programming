3015c0f4bdb3ee4547de673179d89c98aefff269
omoluabi44 <emmanuelogunleye441999@gmail.com>
2023-11-28 14:51:41 +0100

    solved

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,8 +5,8 @@ num = abs(number) % 10
 if number < 0:
     num = - num
 if num > 5:
-    print (f"Last digit of {number:d} is {num} and is greater than 5")
+    print(f"Last digit of {number:d} is {num} and is greater than 5")
 elif num < 6 and num != 0:
-     print (f"Last digit of {number:d} is {num} and is less than 6 and not 0")
+    print(f"Last digit of {number:d} is {num} and is less than 6 and not 0")
 else:
-     print (f"Last digit of {number:d} is {num} and is 0")
+    print(f"Last digit of {number:d} is {num} and is 0")
--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """print the alphabet in lowercase, not follow by line."""
 
-for i in range(97,123):
-    print("{}".format(chr(i)),end="")
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
