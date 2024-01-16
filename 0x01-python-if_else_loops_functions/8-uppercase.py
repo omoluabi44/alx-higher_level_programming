de741e84a27ecf885fcdb5991ee97ec81883c1e8
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-16 20:29:47 +0100

    bugs fixed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,7 @@ def uppercase(str):
         convert_digit = ord(i)
         if convert_digit > 96 and convert_digit < 123:
             is_upper = is_upper + chr(convert_digit - 32)
-        elif i.isdigt():
+        elif i.isdigit():
             is_upper = is_upper + i
         elif i == " ":
             is_upper += " "
