36ca08de46cb006b3d0046711c70745a581172e2
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-18 17:22:07 +0100

    create env, get the total element passed to it

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    sys.argv[0] = "argument"
+    n = len(sys.argv)
+    b = len(sys.argv) - 1
+
+    if b == 0:
+        print("{} {}s.".format(b, sys.argv[0]))
+    elif b == 1:
+        print("{} {}:".format(b, sys.argv[0]))
+    else:
+        print("{} {}s:".format(b, sys.argv[0]))
+    for x in range(1, n):
+        print("{} {}".format(x, sys.argv[x]))
