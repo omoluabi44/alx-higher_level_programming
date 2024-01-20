e92560197cc70e233b779cc0674dfc86352b6f45
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-20 12:46:17 +0100

    solved all task from 1 to  6

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    length = len(my_list) - 1
+    if idx < 0 or idx > length:
+        return my_list
+    else:
+        new_list = []
+        for i in my_list:
+            new_list.append(i)
+        new_list[idx] = element
+        return new_list
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new = ""
+    for i in my_string:
+        if ord(i) != 99 and ord(i) != 67:
+            new += i
+    return (new)
