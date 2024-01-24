e51e52297547d110f8296b97ceda81361a2427b4
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-24 22:44:03 +0100

    adhere to alx requirement

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    new = list(map((lambda x: x * 4), my_list))
+    def mul(num):
+        return num * number
+    new = list(map(mul, my_list))
     return new
