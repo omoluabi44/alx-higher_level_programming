b9ed070cdff2ab8330d1b6ba6e5be3d459ef78bc
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-01-24 20:16:35 +0100

    implement pycode style

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -3,5 +3,5 @@
 def print_sorted_dictionary(a_dictionary):
     sort = sorted(a_dictionary)
     for i in sort:
-        new_dic =(f'{i}: {a_dictionary[i]}')
+        new_dic = (f'{i}: {a_dictionary[i]}')
         print(new_dic)
