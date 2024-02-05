20b6d95448c55ede2022592dae1516170086955f
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-05 18:51:14 +0100

    pycode style

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             x = int(my_list[i])
             print("{:d}".format(x), end="")
             c += 1
-        except (TypeError,ValueError):
+        except (TypeError, ValueError):
             pass
     print()
     return c
