725ba08df8401793e35a28877ad3054581834345
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-06 20:26:46 +0100

    still on 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -46,5 +46,10 @@ class Square:
 
         for _ in range(self.__position[1]):
             print()
+
         for i in range(self.__size):
-            print(" " * self.__position[0] + "#" * self.__size)
+            if self.__position[1] == 0:
+                print(" " * self.__position[0] + "#" * self.__size)
+            else:
+                print("#" * self.__size)
+  
