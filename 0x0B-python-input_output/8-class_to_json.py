67796cfb726a490aafd199e8778230dc51b885c1
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-20 18:08:51 +0100

    task 9

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -5,4 +5,4 @@ this module defines a python class-to-json function """
 
 def class_to_json(obj):
     """ return the dict repr. if of a simple data struc."""
-        return obj.__dict__
+    return obj.__dict__
