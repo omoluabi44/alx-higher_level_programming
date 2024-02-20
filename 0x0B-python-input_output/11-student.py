72a5c7ed594401d7e8c39af88ae35f371610e56f
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-20 19:04:30 +0100

    task 12 done

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -15,5 +15,7 @@ class Student:
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
       def reload_from_json(self, json):
+          """Replaces all attributes of the Student
+          """
           for key, value in json.items():
               setattr(self, key, value)
