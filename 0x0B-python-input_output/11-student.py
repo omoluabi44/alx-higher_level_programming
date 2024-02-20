573e98c9a95a68d68c892e9adc8575e4004c931c
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-02-20 19:06:23 +0100

    finally task 12

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,12 +10,17 @@ class Student:
         self.age = age
     """ return the dict """
     def to_json(self, attrs=None):
+        """Gets a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list
+        """
         if isinstance(attrs, list) and \
            all(isinstance(ele, str) for ele in attrs):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
-      def reload_from_json(self, json):
-          """Replaces all attributes of the Student
-          """
-          for key, value in json.items():
-              setattr(self, key, value)
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the Student
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
