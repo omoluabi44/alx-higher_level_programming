ee6022d58bacc97c3ade5b299bff8ca9f41f0cf9
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-03-26 18:57:14 +0100

    task 9, insert multiple data to table

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,8 @@
+-- create a full name with data
+
+CREATE TABLE IF NOT EXISTS second_tabel(id INT, name VARCHAR(256), score INT );
+INSERT INTO second_table(id, name, score) VALUES
+       (1, 'John',10),
+       (2, 'Alex', 3),
+       (3, 'Bob', 14),
+       (4, 'George', 8);
