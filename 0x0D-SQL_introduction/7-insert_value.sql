d9db7e251ca6c724a73c77083d25f7967e42ba7d
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-03-26 18:28:57 +0100

    task 8, corrcted output

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- INSERT INTO ROW
-INSERT INTO first_table(id, name) value(89, "Best School");
+INSERT INTO first_table(id, name) value(9, "Best School");
--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,3 @@
 -- count the number of id in a row
 
-SELECT COUNT(id = 89) AS id_count FROM first_table WHERE id IS NOT NULL;
+SELECT COUNT(id) AS id_count FROM first_table WHERE id = 89;
