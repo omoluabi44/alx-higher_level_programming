c562b24b7340387188005b38bc874bc1e698d2e2
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-03-27 19:10:13 +0100

    task 11

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,7 @@
--- Select shows and their corresponding genre_ids from hbtn_0d_tvshows database
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-WHERE tv_show_genres.genre_id IS NOT NULL
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+-- Lists all shows in hbtn_0d_tvshows that have at least one genre linked.
+-- Records are sorted by ascending tv_shows.title and tv_show_genres.genre_id.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+          INNER JOIN `tv_show_genres` AS g
+	  	ON s.`id` = g.`show_id`
+		 ORDER BY s.`title`, g.`genre_id`;
