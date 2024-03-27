f58baff4097e08bf5a5c34668b632ff73603878e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-03-27 19:03:44 +0100

    task 11

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Select shows and their corresponding genre_ids from hbtn_0d_tvshows database
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+WHERE tv_show_genres.genre_id IS NOT NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
