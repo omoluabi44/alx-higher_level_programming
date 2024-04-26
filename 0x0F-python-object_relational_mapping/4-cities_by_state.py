28595dbb94bb446297b6d7cad7d1389d03f102ee
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-04-26 18:39:32 +0100

    task 5 solved

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = """
-    SELECT states.name, cities.id, cities.name
+    SELECT cities.id, cities.name, states.name
     FROM cities
     INNER JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC;
