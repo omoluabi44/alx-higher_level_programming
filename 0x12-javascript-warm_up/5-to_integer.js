12482260dd2a71cb2ffcabdcae721249be5542fa
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-19 19:51:01 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,4 +1,7 @@
 #!/usr/bin/node
-
+/*
 const argLine = Math.floor(Number(process.argv[2]));
 console.log(isNaN(argLine) ? 'Not a Number' : `My number: ${num}`);
+*/
+const num = Math.floor(Number(process.argv[2]));
+console.log(isNaN(num) ? 'Not a number' : `My number: ${num}`);
