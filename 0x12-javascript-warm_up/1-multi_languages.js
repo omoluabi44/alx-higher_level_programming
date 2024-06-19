4c414766ce5ad59cb42ba094e99bc8ebe11c5373
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-19 18:49:41 +0100

    task 2

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -4,6 +4,4 @@ const myVar = 'C is fun';
 const myVar1 = 'Python is cool';
 const myVar2 = 'JavaScript is amazing';
 
-console.log(myVar);
-console.log(myVar1);
-console.log(myVar2);
+console.log(myVar + '\n' + myVar1 + '\n' +  myVar2);
