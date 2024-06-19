ef5507af6dd8a485f2e8df64e41e585303b71106
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-19 19:40:32 +0100

    task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,7 +3,7 @@
 const argLine = process.argv.slice(2);
 
 if (argLine) {
-  const intFromStr = parseInt(argLine[0]);
+  const intFromStr = Math.floor(argLine[0]);
   if (isNaN(intFromStr)) {
     console.log('Not a Number');
   } else {
