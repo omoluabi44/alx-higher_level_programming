6a9137d2dad78642051c2b8c6ec80db22732b0f2
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-20 18:14:22 +0100

    task 4

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,1 +1,20 @@
 #!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let line = '';
+      for (let j = 0; j < this.width; j++) {
+        line += 'X';
+      }
+      console.log(line);
+    }
+  }
+}
+module.exports = Rectangle;
