cf1b1f176c6adfd22ff20a2238cec7ca4b628a1f
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-21 17:39:29 +0100

    codestyles

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
         passwd=usrpsw,
         db=dbnm)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE\
+    name LIKE BINARY 'N%' ORDER BY id ASC")
 
     row = cur.fetchall()
 
