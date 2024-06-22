5f3f8b96dbb2429ea832009b82a0e0df9bca4f7e
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-22 19:18:06 +0100

    still on 11

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+""" prints the State object with the name passed as argument from the database
+"""
 import sys
+from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+
 
 if __name__ == "__main__":
-    usrnm = sys.argv[1]
-    usrpsw = sys.argv[2]
-    dbnm = sys.argv[3]
-    stnm = sys.argv[4]
-
-    connection_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
-        .format(usrnm, usrpsw, dbnm)
-    engine = create_engine(connection_string)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
