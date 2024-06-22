f705c391b115882039a3a534aa7f8d0d1b963422
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-06-22 20:14:33 +0100

    task 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    usrnm = sys.argv[1]
+    usrpsw = sys.argv[2]
+    dbnm = sys.argv[3]
+
+    connection_string = 'mysql+mysqldb://{}:{}@localhost:3306/{}'\
+        .format(usrnm, usrpsw, dbnm)
+    engine = create_engine(connection_string)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = \
+        session.query(State).filter(State.name.like('%a%')).all()
+
+    for i in query:
+        session.delete(i)
+    session.commit()
