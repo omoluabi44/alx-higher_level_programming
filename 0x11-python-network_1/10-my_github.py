86e21cd08a330027df6dc226c26d5fd6606016a8
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-05 20:24:42 +0100

    lst task

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" POST method """
+
+import sys
+import requests
+from requests.auth import HTTPBasicAuth
+
+if __name__ == "__main__":
+    auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    r = requests.get("https://api.github.com/user", auth=auth)
+    print(r.json().get("id"))
