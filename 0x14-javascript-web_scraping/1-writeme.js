b41913600012c0252e557a743aa897db4851bc66
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-30 15:32:50 +0100

    task 2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs')
+const filepath = process.argv[2];
+const str = process.argv[3];
+
+fs.writeFile(filepath, str, 'utf8', (err) => {
+  if (err){
+    console.error(err);
+    return;
+    }
+});
