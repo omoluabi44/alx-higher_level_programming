b7b66956e0c7938904c71152fba9b229d5ea6034
omoluabi44 <emmanuelogunleye441999@gmail.com>
2024-07-31 10:02:16 +0100

    task 3

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const filepath = process.argv[2];
 
-fs.readFile(filepath,'utf8', (err,data) => {
-  if (err){
+fs.readFile(filepath, 'utf8', (err, data) => {
+  if (err) {
     console.error(err);
     return;
-    }
+  }
   console.log(data);
 });
--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-const fs = require('fs')
+const fs = require('fs');
 const filepath = process.argv[2];
 const str = process.argv[3];
 
 fs.writeFile(filepath, str, 'utf8', (err) => {
-  if (err){
+  if (err) {
     console.error(err);
-    return;
-    }
+  }
 });
